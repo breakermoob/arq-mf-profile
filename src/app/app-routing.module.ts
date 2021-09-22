15aea00824b7fcd036bdf290399130b57faabfca
lion <breakermoob@hotmail.com>
2021-09-22 03:10:54 -0500

    First commit - base Angular profile config with single spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AppComponent },
+  { path: "**", component: EmptyRouteComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
 })
 export class AppRoutingModule { }
